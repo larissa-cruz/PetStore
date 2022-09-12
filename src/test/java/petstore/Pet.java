64b5b7faf790dd248ec0c22f81df42896c9d393b
Larissa <larissacruz_01@hotmail.com>
2022-09-12 16:16:19 -0300

    Incluída assertivas para o body do teste IncluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 //3 - Classe
 public class Pet {
@@ -39,6 +40,8 @@ public class Pet {
         .then()//Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Atena")) //linha de validação, verifica se o nome do cachorro é Snoopy.
+                .body("status", is("available")) //linha de validação, verifica se o status é avaliable.
         ;
     }
 }
