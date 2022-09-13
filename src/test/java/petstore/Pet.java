6e472805729143a20dc5a6d3424decde0166e04d
Larissa <larissacruz_01@hotmail.com>
2022-09-12 21:44:55 -0300

    Finalizado o script ExcluirPet (Delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -85,4 +85,23 @@ public class Pet {
                 .body("status", is("sold"))
         ;
     }
+
+    @Test(priority = 4)
+    public void excluirPet() {
+        String petId = "1608199527";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
+        ;
+    }
 }
